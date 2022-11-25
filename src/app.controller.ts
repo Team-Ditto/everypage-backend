c2a003f627e90f28b169f724ba6d8864f50d1f13
Ashim Karki <aceem123@gmail.com>
2022-11-24 17:55:19 -0800

    removed static file implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-    @Get()
-    @Render('index')
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    root() {}
-}
+export class AppController {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { RequestMethod, ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
@@ -11,20 +9,14 @@ import { EnvironmentVariables } from './env.validation';
 declare const module: any;
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService<EnvironmentVariables>);
 
     app.use(helmet());
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-    app.useStaticAssets(join(__dirname, '..', 'public'));
-    app.setBaseViewsDir(join(__dirname, '..', 'views'));
-    app.setViewEngine('hbs');
-
-    app.setGlobalPrefix('api/v1', {
-        exclude: [{ path: '', method: RequestMethod.GET }],
-    });
+    app.setGlobalPrefix('api/v1');
 
     await app.listen(configService.get<number>('PORT'));
 
